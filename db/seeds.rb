da3c28d9b407bac72b4e1b4de379f0d391471afa
Estauti <estautib@gmail.com>
2019-12-29 20:04:24 -0300

    Add tags creating on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Started Seeds'
+
+puts '-- Creating Tags'
+5.times do
+  Tag.create(name: FFaker::Lorem.word)
+end
+puts "-- Tags Created"
+puts ''
+
+puts 'Finished Seeds'
