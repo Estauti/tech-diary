65a04eed983e0f9b3baa42a43e51c2563e8b91b7
Estauti <estautib@gmail.com>
2019-12-29 19:54:58 -0300

    Add model tests to Tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'valid cases' do
+    it 'is valid on creation' do
+      tag = build(:tag)
+
+      expect(tag).to be_valid
+    end
+  end
+
+  context 'invalid cases' do
+    it 'throws error when repeating name' do
+      tag_name = FFaker::Lorem.word
+      create(:tag, name: tag_name)
+
+      expect {
+        create(:tag, name: tag_name)
+      }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+
+    it 'is invalid with empty name' do
+      tag = build(:tag, name: '')
+
+      expect(tag).to be_invalid
+    end
+
+    it 'throws error with null name' do
+      expect {
+        create(:tag, name: nil)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
