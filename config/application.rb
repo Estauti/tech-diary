0a7676395ccef0b8440e71fc455e49d4b4e018d1
Estauti <estautib@gmail.com>
2019-12-29 19:01:47 -0300

    Create Tag scaffold

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module DiscoveryHub
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.controller_specs false
+      g.routing_specs false
+    end
   end
 end
---
+++ spec/requests/tags_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Tags", type: :request do
+  describe "GET /tags" do
+    it "works! (now write some real specs)" do
+      get tags_path
+      
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
