b7ccbc3bd5e9f951bb4a184a9fb7c4c4ccd2f3e6
Estauti <estautib@gmail.com>
2020-01-02 13:04:17 -0300

    User authentication on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :authenticate_user!
 
   def index
     render json: User.all, status: :ok
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-    
+
   rescue ActiveRecord::RecordNotFound => e
     render json: { error: e.message }, status: :not_found
   end
